f1a8224caa813805699a8babba14b2003dca110f
Markim <ms79723@gmail.com>
2021-03-09 01:32:29 -0600

    Create index route

--- src/app.js
+++ src/app.js
@@ -8,15 +8,14 @@ const options = require("./model/source-options");
 
 const searchRouter = require('./routes/search.route');
 const productRouter = require('./routes/product.route');
+const indexRouter = require('./routes/index.route');
 
 const app = express();
 app.use(bodyParser.json());
 
+app.use("/", indexRouter);
 app.use("/search", searchRouter);
 app.use("/product", productRouter);
-app.get("/", (request, response) => {
-  response.send("Welcome to hpb-scrape");
-});
 
 if (process.env.LOCAL_ENABLED) {
   app.listen(3000, () => console.log('Running on port 3000'));
