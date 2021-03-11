27baf8cd44a346311f9f997b6b6a3018b959c7df
Markim Shaw <djsalinger67@live.com>
2021-03-11 03:43:59 -0600

    Update (#5)
    Co-authored-by: Markim <ms79723@gmail.com>

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (request, response) => {
-  response.send('Welcome to hpb-scrape');
+  response.send('Welcome to hpb-scrape!');
 });
 
 module.exports = router;
